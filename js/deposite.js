eae739b47186d9784bb9365a874e4921f89a4114
Sawon-52 <mehedihasansawon.52@gmail.com>
2023-08-20 12:12:29 +0600

    added set function which is display doposite total ammount

--- js/deposite.js
+++ js/deposite.js
@@ -2,6 +2,7 @@ function getInputValue(inputID){
     const selectInputField = document.getElementById(inputID);
     const newAmmountString = selectInputField.value;
     const newAmmount = parseFloat(newAmmountString);
+    selectInputField.value ='';
     return newAmmount;
 }
 
@@ -10,18 +11,30 @@ function getTextValue(elementId){
     const previousAmmountString = previousTextField.innerText;
     const previousAmmount = parseFloat(previousAmmountString);
     return previousAmmount;
-
 }
+
+function setTotalValue(position, newValue){
+    const textPosition = document.getElementById(position);
+    textPosition.innerText = newValue;
+}
+
+
 
 
 document.getElementById('deposite-btn').addEventListener('click', function(){
 
     // take diposite value and call function to process it
-    const newdipositeValue = getInputValue('deposite-form');
-    // console.log(newdipositeValue);
+    const newDipositeValue = getInputValue('deposite-form');
+    // console.log(newDipositeValue);
 
-    const previousdipositeValue = getTextValue('deposite-amount');
-    console.log(previousdipositeValue);
+    const previousDipositeValue = getTextValue('deposite-amount');
+    // console.log(previousDipositeValue);
+    
+    // total diposite amount
+    const totalDiposite = previousDipositeValue + newDipositeValue;
+    // console.log(totalDiposite);
 
+    // set the new ammount in deposite field
 
+    const displayDiposite = setTotalValue('deposite-amount',totalDiposite);
 })
